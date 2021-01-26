4b6bb98cc7a1579f0f2c0ab9f72fecfa74513902
Dustin <dustin_chu@hotmail.com>
2021-01-26 21:46:34 +0800

    delete hash (#)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'max_size.dart';
 import 'mix_size.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(MyApp());
 }
 
